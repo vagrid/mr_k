ded2e156e82498612e46967cf25e7ae317bda0bc
Anthony Rey <anthonyrey.simonnot@gmail.com>
2019-09-28 17:48:35 +0200

    mission css improvements | dealing with the django template and model of mission app

--- mission/models.py
+++ mission/models.py
@@ -25,23 +25,24 @@ class Location(models.Model):
 class Mission(models.Model):
     """
     """ 
-    title           = models.CharField(max_length = 100)
-    slug            = models.SlugField(max_length = 100, unique = True) 
-    location        = models.ForeignKey(Location, related_name = "city", on_delete = models.CASCADE)  
-    description     = models.TextField() 
-    picture         = models.ImageField(upload_to = "images")
+    title               = models.CharField(max_length = 100)
+    slug                = models.SlugField(max_length = 100, unique = True) 
+    location            = models.ForeignKey(Location, related_name = "city", on_delete = models.CASCADE)  
+    description         = models.TextField() 
+    picture             = models.ImageField(upload_to = "images")
+    polaroid_caption    = models.CharField(max_length = 100)
     #
-    difficulty      = models.IntegerField(default = 1, validators = [MaxValueValidator(5), MinValueValidator(1)])
-    logic           = models.IntegerField(default = 1, validators = [MaxValueValidator(5), MinValueValidator(1)])
-    handling        = models.IntegerField(default = 1, validators = [MaxValueValidator(5), MinValueValidator(1)])
-    search          = models.IntegerField(default = 1, validators = [MaxValueValidator(5), MinValueValidator(1)])
+    difficulty          = models.IntegerField(default = 1, validators = [MaxValueValidator(5), MinValueValidator(1)])
+    logic               = models.IntegerField(default = 1, validators = [MaxValueValidator(5), MinValueValidator(1)])
+    handling            = models.IntegerField(default = 1, validators = [MaxValueValidator(5), MinValueValidator(1)])
+    search              = models.IntegerField(default = 1, validators = [MaxValueValidator(5), MinValueValidator(1)])
     #
-    success         = models.IntegerField(default = 1, validators = [MaxValueValidator(100), MinValueValidator(0)])
-    min_players     = models.IntegerField(default = 1, validators = [MaxValueValidator(100), MinValueValidator(1)])
-    max_players     = models.IntegerField(default = 1, validators = [MaxValueValidator(100), MinValueValidator(1)])
-    min_age         = models.IntegerField(default = 1, validators = [MaxValueValidator(100), MinValueValidator(1)])
-    max_age         = models.IntegerField(default = 1, validators = [MaxValueValidator(100), MinValueValidator(1)])
-    duration        = models.IntegerField(default = 1, validators = [MaxValueValidator(180), MinValueValidator(5)])
+    success             = models.IntegerField(default = 1, validators = [MaxValueValidator(100), MinValueValidator(0)])
+    min_players         = models.IntegerField(default = 1, validators = [MaxValueValidator(100), MinValueValidator(1)])
+    max_players         = models.IntegerField(default = 1, validators = [MaxValueValidator(100), MinValueValidator(1)])
+    min_age             = models.IntegerField(default = 1, validators = [MaxValueValidator(100), MinValueValidator(1)])
+    max_age             = models.IntegerField(default = 1, validators = [MaxValueValidator(100), MinValueValidator(1)])
+    duration            = models.IntegerField(default = 1, validators = [MaxValueValidator(180), MinValueValidator(5)])
 
 
     # 
--- mission/views.py
+++ mission/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 from django.views.generic import View
 from .models import Location, Mission
 
+from collections import OrderedDict
+
 class MissionsView(View):
     """
     """
@@ -13,17 +15,33 @@ class MissionsView(View):
         """
         """
         # 
-        template_name       = "mission/missions.html"
+        template_name           = "mission/missions.html"
         #
-        location            = slug 
+        location                = slug 
         # 
-        locations           = Location.objects.all()
+        locations               = Location.objects.all()
         # 
-        missions            = Mission.objects.all()
+        missions                = Mission.objects.all()
         #
-        mission_description = []
+        mission_gauge = dict()
         # 
-        context             = {"location":location, "locations":locations, "missions":missions, "description":mission_description} 
+        for mission in missions:
+            mission_gauge[mission.title]                = OrderedDict()#dict()
+            mission_gauge[mission.title]["difficulty"]  = mission.difficulty 
+            mission_gauge[mission.title]["logic"]       = mission.logic 
+            mission_gauge[mission.title]["handling"]    = mission.handling 
+            mission_gauge[mission.title]["search"]      = mission.search 
+        #
+        mission_information = dict()
+        # 
+        for mission in missions:
+            mission_information[mission.title]                = OrderedDict()#dict()
+            mission_information[mission.title]["success"]     = mission.success 
+            mission_information[mission.title]["players"]     = [mission.min_players, mission.max_players] 
+            mission_information[mission.title]["age"]         = [mission.min_age, mission.max_age] 
+            mission_information[mission.title]["duration"]    = mission.duration 
+        # 
+        context             = {"location":location, "locations":locations, "missions":missions, "gauge":mission_gauge, "information":mission_information, "range":range(1,6)} 
         #
         return render(request, template_name, context)
 
